17e05ced3f5eada4e5eb0a6f2f1d3ba2952274b0
Gustavo Peregrino <gustavo.peregrino@estudante.ufjf.br>
2025-09-25 11:15:34 -0300

    Att final arquivos js

--- frontend/login.js
+++ frontend/login.js
@@ -3,13 +3,16 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('error-message');
 
+// URL da sua API online
+const API_URL = 'https://gestao-api-aluno.onrender.com';
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     errorMessage.textContent = '';
     const email = emailInput.value;
     const password = passwordInput.value;
     try {
-        const response = await fetch('http://127.0.0.1:3001/login', {
+        const response = await fetch(`${API_URL}/login`, { // <-- ALTERADO AQUI
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: email, senha: password }),
